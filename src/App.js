5944c78c6c659a252f3b8ed1928a1eb150633cf7
ubuntu <ubuntu@ubun2004.linuxvmimages.local>
2020-07-07 16:31:37 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye There!
-          Edit <code>src/App.js</code> and save to reload.
+          CIAO change to the feature branch
         </p>
         <a
           className="App-link"
